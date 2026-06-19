83e25eb9d8b76b3fd3a34abf47b97d5581124050
gad-biton <gadb633@gmail.com>
2026-06-19 17:32:57 +0300

    added Practice 2

--- Self_Learn_python.py
+++ Self_Learn_python.py
@@ -8,11 +8,20 @@
 #  == This compares whether the content on both sides is equal and does not contradict each other and is It checks whether it is exactly the same variable not only if the content does not contradic
 # 4
 # Because the None command is stored in the computer's memory in one cell and not each time, the way to compare it is to check whether it is the same address of a location in memory or not, and therefore is is better.
+# Practice
+# 1
 a = 12
 b = 10
 print(bin(a))
 print(bin(b))
 print(a ^ b)
+# 2
+a = 12
+b = 10
+print(a & b , a | b)
+print(bin(8))
+print(bin(14))
+
 
 
 
