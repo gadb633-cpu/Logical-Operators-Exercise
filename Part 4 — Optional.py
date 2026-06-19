be8f924e67ac148979fc2869e626363f572e461b
gad-biton <gadb633@gmail.com>
2026-06-19 18:14:44 +0300

    added optional Part 4

---
+++ Part 4 — Optional.py
@@ -0,0 +1,11 @@
+a = True
+b = False
+print((a or b) and (a and b) and not (a or b) and (a and b))
+num = 8
+apply = num << 1 , num << 2 , num >> 1 , num >> 2
+result_apply = print(apply)
+print(apply)
+print(f"num << 1 , num << 2 , num >> 1 , num >> 2 = apply (bin(result_apply)")
+
+
+
